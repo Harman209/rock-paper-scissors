18a8026007f671e1222b336ca88b9c0d89aa22c4
Harman Kajla <63535022+Harman209@users.noreply.github.com>
2021-06-22 23:11:40 +0530

    Added Main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+choices = ['r', 'p', 's']
+
+
+player = input('Let\'s play rock paper scissors.\n \'r\' for rock, \'p\' for paper and \'s\' for scissors')
+computer = random.choice(choices)
+
+# r > s, s > p, p > r 
+
+def result(computer, player):
+    if computer == player:
+        print('It is a tie!!')
+    if (player == 'r' and computer == 's') and (player == 's' and computer == 'p') or (player == 'p' and computer == 'r'):
+        print('You Won!!')
+    else:
+        print('You Lost :(')
+
+if __name__ == '__main__':
+    print('You played ' + player + ' computer played ' + computer)
+    result(computer, player)
